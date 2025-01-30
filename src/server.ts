b65248b4bb0ddada6656e2ee184123c5224d67e9
Bruno Correa <brunosantos6ft@gmail.com>
2025-01-30 20:07:14 -0300

    feat: implement clustering to leverage multiple cpu cores

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,51 @@
 import 'reflect-metadata'
-
+import cluster from 'cluster'
+import os from 'os'
 import { app } from '@/app'
 import { initializeDataSource } from '@/config/typeorm'
 import { env } from '@/config/env'
 
-app.register(async () => await initializeDataSource())
+const startServer = async () => {
+  await initializeDataSource()
 
-app
-  .listen({
-    host: '0.0.0.0', // Listens on all IPv4 addresses / alt: 127.0.0.1
-    port: env.PORT,
+  app
+    .listen({
+      host: '0.0.0.0', // Listens on all IPv4 addresses / alt: 127.0.0.1
+      port: env.PORT,
+    })
+    .then(address => {
+      console.log(
+        `Worker ${process.pid} started. Server running on ${address} @ ${env.NODE_ENV}`
+      )
+    })
+    .catch(error => {
+      console.error('Server failed to start:', error)
+      process.exit(1)
+    })
+}
+
+// Clustering logic
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`)
+
+  // Fork workers based on the number of CPU cores
+  const numCPUs = os.cpus().length
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  // Handle worker exit events
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    )
+    console.log('Forking a new worker...')
+    cluster.fork() // Replace the dead worker
   })
-  .then(address => {
-    console.log(`HTTP Server running on ${address} @ ${env.NODE_ENV}`)
-  })
-  .catch(error => {
-    console.error(error)
+} else {
+  // Workers will start the server
+  startServer().catch(error => {
+    console.error('Failed to start server:', error)
     process.exit(1)
   })
+}
